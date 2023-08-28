cb7a6568dc3faef606ca337ff06f06d16a2fc73c
Ayan <ayan@elsewhen.com>
2023-08-28 22:02:51 +0100

    style: adds globabl styles and updates tsconfig file

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import Title from './components/Title'
+
+const App: FC = () => {
+    return (
+        <div>
+            <Title />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/Title.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+const Title: FC = () => {
+    return <h1>find coffee near me</h1>
+}
+
+export default Title
