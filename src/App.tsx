e79879ef9412b8f74bfc9b2bbdb89804d88e7fd7
Ayan <ayan@elsewhen.com>
2023-09-10 22:07:52 +0100

    feat: add form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react'
 import Title from './components/Title'
+import InputField from './components/Input/Input'
 
 const App: FC = () => {
     return (
         <div>
             <Title />
+            <InputField />
         </div>
     )
 }
